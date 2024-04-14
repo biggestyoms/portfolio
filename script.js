88e5908ff8dac20c1b58e8ac7cca3e2b91c7a653
biggestyoms <oladimejiyomi11@gmail.com>
2024-04-13 22:15:20 -0400

    new java

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 let menuIcon = document.querySelector("#menu-icon");
 let navBar = document.querySelector('.navbar');
-let sections = document.querySelector('section');
-let navLinks = document.querySelector('header nav a');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    section.forEach (sec => {
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if( top >= offset && top < offset + height){
+        if(top >= offset && top < offset + height){
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a
-                [href*=' + id + ']').classList.add
+                document.querySelector('header nav a[href*=' + id + ' ]').classList.add
                 ('active')
+
             })
         }
     })
 }
-menuIcon.onClick = () => {
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
-    navBar.classList.toggle('active')
+    navBar.classList.toggle('active');
 }
+
+
